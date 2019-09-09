66f002a77d678ff379b8fb90b778ac61d5ced3b6
kit-lec <kit_lec@hotmail.com>
2019-09-09 22:17:11 +0900

    190909

---
+++ Lec01_HelloJava/src/com/lec/java/hello1/HelloMain.java
@@ -0,0 +1,11 @@
+package com.lec.java.hello1;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요 Java");
+		System.out.println("안녕하세요 Java2");
+		System.out.println("안녕하세요 Java3");
+	}
+
+}
---
+++ Lec02_FirstJava/src/com/lec/firstjava1/Firstjava.java
@@ -0,0 +1,10 @@
+package com.lec.firstjava1;
+
+public class Firstjava {
+
+	public static void main(String[] args) {
+		System.out.println("FirstJava");  // CTRL + F11
+
+	}
+
+}
